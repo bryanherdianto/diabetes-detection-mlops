48eb984dcd8e1c35002f4111e1f2f45e3017f4d0
Bryan Herdianto <bryan.herdianto17@gmail.com>
2026-01-02 23:33:13 +0700

    edited prometheus yml file and modified testing ipynb

--- pipeline.py
+++ pipeline.py
@@ -1,6 +1,6 @@
 import os
 
-os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"
 
 from typing import Text
 from absl import logging
@@ -9,29 +9,27 @@ from tfx.orchestration.beam.beam_dag_runner import BeamDagRunner
 from modules.components import init_components
 
 PIPELINE_NAME = "diabetes-pipeline"
- 
+
 # Files for pipeline inputs
 DATA_ROOT = "data"
 TRANSFORM_MODULE_FILE = "modules/diabetes_transform.py"
 TRAINER_MODULE_FILE = "modules/diabetes_trainer.py"
- 
+
 # Files for pipeline outputs
 OUTPUT_BASE = "output"
-serving_model_dir = os.path.join(OUTPUT_BASE, 'serving_model')
+serving_model_dir = os.path.join(OUTPUT_BASE, "serving_model")
 pipeline_root = os.path.join(OUTPUT_BASE, PIPELINE_NAME)
 metadata_path = os.path.join(pipeline_root, "metadata.sqlite")
 
-def init_local_pipeline(
-    components, pipeline_root: Text
-) -> pipeline.Pipeline:
-    
+
+def init_local_pipeline(components, pipeline_root: Text) -> pipeline.Pipeline:
     logging.info(f"Pipeline root set to: {pipeline_root}")
     beam_args = [
         "--direct_running_mode=multi_processing",
         # auto-detect based on on the number of CPUs available during execution time.
-        "--direct_num_workers=0" 
+        "--direct_num_workers=0",
     ]
-    
+
     return pipeline.Pipeline(
         pipeline_name=PIPELINE_NAME,
         pipeline_root=pipeline_root,
@@ -40,11 +38,12 @@ def init_local_pipeline(
         metadata_connection_config=metadata.sqlite_metadata_connection_config(
             metadata_path
         ),
-        beam_pipeline_args=beam_args
+        beam_pipeline_args=beam_args,
     )
 
+
 logging.set_verbosity(logging.INFO)
-        
+
 components = init_components(
     DATA_ROOT,
     training_module=TRAINER_MODULE_FILE,
